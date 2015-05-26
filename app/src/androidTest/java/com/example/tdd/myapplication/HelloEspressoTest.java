960b2d522e07172a7663e34eb6c29a0541b32df3
unknown <Pongrapee.P@LN5700183.progresssoftware.co.th>
2015-05-26 15:01:16 +0700

    edit2

--- app/src/androidTest/java/com/example/tdd/myapplication/HelloEspressoTest.java
+++ app/src/androidTest/java/com/example/tdd/myapplication/HelloEspressoTest.java
@@ -10,6 +10,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 import javax.annotation.security.PermitAll;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -24,15 +28,29 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(Parameterized.class)
 @LargeTest
-public class HelloEspressoTest {
+public  class HelloEspressoTest {
+
+    private final String celsius;
+    private final String fahrenheit;
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
+    @Parameters
+    public  static Iterable<Object[]>data(){
+        return Arrays.asList(new Object[][]{
+                {"0","32.0"}
+        });
+    }
 
-    @Test public void convert_temperator_from_0_celsius_should_32_fahrenheit(){
-        onView(withId(R.id.txtCelsius)).perform(typeText("0"));
+    public HelloEspressoTest(String celsius, String fahrenheit){
+        this.celsius = celsius;
+        this.fahrenheit = fahrenheit;
+    }
+    @Test
+    public void convert_temperator_from_0_celsius_should_32_fahrenheit(){
+        onView(withId(R.id.txtCelsius)).perform(typeText(this.celsius));
 
-        onView(withId(R.id.txtFah)).check(matches(withText("32.0")));
+        onView(withId(R.id.txtFah)).check(matches(withText(this.fahrenheit)));
     }
 
 }
